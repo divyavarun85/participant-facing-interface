90256ba80b6046869bc0c166df37128744437e88
Divya Varun <divyavarun85@gmail.com>
2026-02-12 17:00:15 -0500

    burger button bug  - reposnsive layout

--- src/components/EnvironmentalMap.vue
+++ src/components/EnvironmentalMap.vue
@@ -23,7 +23,7 @@
             </div>
         </header>
         <div class="environmental-map-container">
-            <button v-if="!sidebarOpen" class="mobile-menu-toggle" @click="sidebarOpen = true" aria-label="Open menu">
+            <button v-if="!sidebarOpen && !selectedHexFeature" class="mobile-menu-toggle" @click="sidebarOpen = true" aria-label="Open menu">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 12H21M3 6H21M3 18H21" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
                 </svg>
